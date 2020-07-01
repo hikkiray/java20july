5fc5528d8f9b37e772d8a19425b31b734d10d1a1
Lev <levparygin3@gmail.com>
2020-07-01 21:03:46 +0600

    Task7

---
+++ src/com/company/Task7.java
@@ -0,0 +1,34 @@
+package com.company;
+import java.util.Scanner;
+public class Task7 {
+    public static void main(String[] args){
+        System.out.print("Введите целые числа разделенные символом ;  :");
+        Scanner In = new Scanner(System.in);
+        String data = In.nextLine();
+        if (data.isEmpty()){
+            System.out.print("Исходная строка пуста");
+            return;
+        }
+        String[] arr = data.split(";");
+        int[] num = new int[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            num[i] = Integer.parseInt(arr[i]);
+        }
+        int temp = 1;
+        boolean isSort = true;
+        for (int i = 1; i < arr.length; i++){
+            if(num[i] >= num[i-1]){
+                temp +=1;
+            }
+
+            else {
+                System.out.print("Элемент со значением " + num[i] + " на индексе " + temp + " нарушает закономерность");
+                isSort = false;
+            }
+
+        }
+        if (isSort)
+            System.out.print("Значения отсортированы по возрастанию");
+
+    }
+}
