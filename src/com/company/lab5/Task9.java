d4640d5402ea7fd2cdf2494e32d9a3ef9131779c
Lev <levparygin3@gmail.com>
2020-07-07 02:16:38 +0600

    Task 9 LAB 5

---
+++ src/com/company/lab5/Task9.java
@@ -0,0 +1,41 @@
+package com.company.lab5;
+import java.util.Scanner;
+public class Task9 {
+    public static void main(String[] args){
+        /**
+         * Считывание данных пользователя
+         */
+        System.out.print("Введите Xa :");
+        Scanner In = new Scanner(System.in);
+        int Xa = In.nextInt();
+        System.out.print("Введите Xb :");
+        int Xb = In.nextInt();
+        System.out.print("Введите Ya :");
+        int Ya = In.nextInt();
+        System.out.print("Введите Yb :");
+        int Yb = In.nextInt();
+
+        int delx = Xb - Xa; //delata x
+        int dely = Yb - Ya; //delat y
+        int quater = 1;
+        double angle = 0;
+        double r = Math.atan(dely/delx); // Находим arctg  =r
+        int horizontal = delx*delx + dely*dely; // Определяем горизонатльное положение
+        if (delx >= 0 && dely > 0){
+            angle = r;
+            System.out.println("\n"+ " Четверть :" +quater+ "\n"+" Дирекционный угол :" +Math.toDegrees(angle)+"\n"+" Горизонтальное проложение :" +Math.sqrt(horizontal));
+        }else if (delx < 0 && dely >= 0){
+            angle = 180 - r;
+            quater = 2;
+            System.out.println("\n"+ " Четверть :" +quater+ "\n"+" Дирекционный угол :" +Math.toDegrees(angle)+"\n"+" Горизонтальное проложение :" +Math.sqrt(horizontal));
+        }else if (delx <= 0 && dely < 0){
+            quater =3;
+            angle = 180 +r;
+            System.out.println("\n"+ " Четверть :" +quater+ "\n"+" Дирекционный угол :" +Math.toDegrees(angle)+"\n"+" Горизонтальное проложение :" +Math.sqrt(horizontal));
+        }else if (delx > 0 && dely <=0){
+            quater =4;
+            angle = 360 - r;
+            System.out.println("\n"+ " Четверть :" +quater+ "\n"+" Дирекционный угол :" +Math.toDegrees(angle)+"\n"+" Горизонтальное проложение :" +Math.sqrt(horizontal));
+        }
+    }
+}
