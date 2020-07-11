0d1bbe6e3f3ddfd971ba5cc39d2011d372ae93ca
Lev <levparygin3@gmail.com>
2020-07-11 07:34:06 +0600

    lab 9 Task13

--- src/com/company/lab9/Task13.java
+++ src/com/company/lab9/Task13.java
@@ -2,7 +2,6 @@ package com.company.lab9;
 import java.util.Scanner;
 public class Task13 {
     public static void main(String[] args) {
-        Scanner In = new Scanner(System.in);
 
         /**
          * Сделать два массива с буквами и цифрами
@@ -11,5 +10,24 @@ public class Task13 {
          * Пройтись по строкам массива и заменить 0 на пробел а 1 на #
          */
         System.out.print("Введите координаты :");
+        Scanner In = new Scanner(System.in);
+        String data = In.nextLine();
+        if(data.isEmpty()){
+            System.out.print("Исходная строка пуста");
+            return;
+        }
+        /**
+         * Записываем числа в OY
+         */
+        String[] arr = data.split(";");
+        int [] OY = new int[arr.length];
+        for(int i =1; i < arr.length; i +=2){
+            OY[i] = Integer.parseInt(arr[i]);
+        }
+        String[] OX = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            OX[i] = String.
+        }
+
     }
 }
