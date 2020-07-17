18df88cf38a037ffb00f2c5ceba0b0610bbfd071
Lev <levparygin3@gmail.com>
2020-07-17 16:59:07 +0600

    lab 11 Task11

--- src/com/company/lab11/Task11.java
+++ src/com/company/lab11/Task11.java
@@ -4,8 +4,16 @@ public class Task11 {
     public static void main(String[] args){
         Scanner In = new Scanner(System.in);
         String fragment = In.nextLine();
-        fragment =fragment.replaceAll(" ","");//Заменяем пробелы на пустоту
-        String s = fragment.substring(1, fragment.length()-1);
-        System.out.print(s);
+        int x,y;
+        x = fragment.indexOf("\"");
+        y = fragment.lastIndexOf("\"");
+        if (x==0 && y == fragment.length()){
+            fragment =fragment.replaceAll(" ","");//Заменяем пробелы на пустоту
+            String s = fragment.substring(1, fragment.length()-1);
+            System.out.print(s);
+        }else {
+            System.out.print("Строка должна начинаться и заканчиваться кавычкой");
+        }
+
     }
 }
