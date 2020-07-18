b4147d1267c8a4d7cc480514ad0574dd3f101697
Lev <levparygin3@gmail.com>
2020-07-18 06:02:21 +0600

    lab 11 Task10

--- src/com/company/lab11/Task10.java
+++ src/com/company/lab11/Task10.java
@@ -4,13 +4,12 @@ public class Task10 {
     public static void main(String[] args){
         Scanner In = new Scanner(System.in);
         String data = "abcdacadbacdaabaadc";
-        System.out.print("Введите 2 символа");
+        System.out.println("Введите два символа :");
         String fragment = In.nextLine();
-        int r = 0;
-        for (int i = 0; i < data.length() && r > -1; i++) {
-            System.out.print(r + " ");
-            r = data.indexOf(fragment, r+1);
-
+        if (fragment.length() != 2){
+            System.out.println("Вводимая строка должна содержать ровно 2 символа");
         }
+        data = data.replaceAll(fragment, " ");
+        System.out.println(data);
     }
 }
