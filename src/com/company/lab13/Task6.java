6184e8cb39356dcf78f945066445dfa95f118c5d
q <a@a>
2020-07-21 12:15:37 +0600

    lab 13 task 6

--- src/com/company/lab13/Task6.java
+++ src/com/company/lab13/Task6.java
@@ -5,9 +5,12 @@ import java.util.Random;
 public class Task6 {
     public static void main(String[] args){
         Random rnd = new Random();
-
+        int [] nusms = new int[100];
         for (int i = 0; i < 100; i++) {
-            System.out.print(100 + rnd.nextInt(899));
+            nusms[i] = (100+rnd.nextInt(899) );
+        }
+        for (int i = 0; i < nusms.length ; i++) {
+            System.out.print(nusms[i]);
             System.out.print(" ");
         }
     }
