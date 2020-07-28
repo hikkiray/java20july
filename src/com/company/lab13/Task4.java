d0aab8a6b1be34be58776f834d6e133a058e2173
Lev <levparygin3@gmail.com>
2020-07-29 01:46:56 +0600

    lab 13 Task4

--- src/com/company/lab13/Task4.java
+++ src/com/company/lab13/Task4.java
@@ -29,6 +29,53 @@ public class Task4 {
         int w2 = In.nextInt();
         System.out.print("Height : ");
         int h2 = In.nextInt();
+        /**
+         * Условие пересечения двух прямоугольников
+         * (x2 > x1 - w2) && (x2 < x1+w1) && (y2 > y1-h2) && (y2 < y1+h1)
+         */
+
+        int x3 ,y3,h3,w3;
+        //Находим Y и Height пересечения
+        if (y2 > y1){
+            h3 = h1 - (y2-y1);
+            y3 = (y2 - y1) + y1;
+        }else {
+            h3 = h2 - (y1-y2);
+            y3 = (y1 - y2) + y2;
+        }
+        //Находим X и Width пересечения
+        if (x2 > x1){
+             w3 = w1 - (x2 - x1);
+             x3 = (x2 - x1) + x1;
+        }else {
+             w3 = w2 - (x1 - x2);
+             x3 = (x1 - x2) + x2;
+        }
+        //Проверяем случай, когда один прямоугольник вложен в другой
+        if (w1 > w2 && h1 > h2 ){
+            h3 = h2;
+            w3 = w2;
+        }else if (w2 > w1 && h2 > h1){
+            h3 = h1;
+            w3 = w1;
+        }
+        //Проверяем случай,
+        if (w1 < w2 && x1 > x2)
+            w3 = w1;
+        if (w2 < w1 && x2 > x1)
+            w3 = w2;
+
+        if (h1 < h2 && y1 > y2)
+            h3 = h1;
+        if (h2 < h1 && y2 > y1)
+            h3 = h2;
+        //Тестовый вывод
+        if ((x2 > x1 - w2) && (x2 < x1+w1) && (y2 > y1-h2) && (y2 < y1+h1)){
+            System.out.println("Прямоугольники пересекаются");
+            System.out.println(h3);
+            System.out.println(w3);
+        }
+
 
         print.println("<!DOCTYPE html>\n" +
                 "<html>\n" +
@@ -41,6 +88,7 @@ public class Task4 {
                 "<svg width=\"1920\" height=\"1080\">\n" +
                 "\t<rect x = \""+x1+"\" y = \""+y1+"\" width = \""+w1+"\" height = \""+h1+"\" fill = \"blue\"/>\n" +
                 "\t<rect x = \""+x2+"\" y = \""+y2+"\" width = \""+w2+"\" height = \""+h2+"\" fill = \"red\"/>\n" +
+                "\t<rect x = \""+x3+"\" y = \""+y3+"\" width = \""+w3+"\" height = \""+h3+"\" fill = \"yellow\"/>\n" +
                 "</svg>\n" +
                 "</body>\n" +
                 "</html>");
